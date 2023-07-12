933b9b23f5c017ded8343d3a6d26a25e8a9b8e75
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2023-07-12 20:56:52 +0530

    First Request

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+//available routes
+app.use('/api/auth' , require ('./routes/auth'));
+// app.use('/api/notes' , require ('./routes/notes'));
+
 app.get("/", (req, res) => {
   res.send("Hello World Ishaaa!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
