e3a4bd60a938435e9129870288e93221ccb43690
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2023-07-14 19:34:00 +0530

    Encryption Added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "thisismy_signature!";
 
 //Create a user using: POST "/api/auth/createuser". Doesn't require auth
 
@@ -28,11 +32,20 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this name already exists" });
       }
+      const salt = await bcrypt.genSalt(10); 
+      const secPass =await bcrypt.hash(req.body.password,salt) ;
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
+      const data = {
+        user:{
+          id : user.id
+        }
+      }
+      const authToken =  jwt.sign(data,JWT_SECRET);
+      res.json({authToken});
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Some error occurred");
