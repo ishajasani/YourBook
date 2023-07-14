8a7ed58567e0514d3bd16623d34e02edfae21aa1
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2023-07-14 17:51:54 +0530

    Validation Added

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/?directConnection=true";
+const mongoURI = "mongodb://localhost:27017/yourbook?directConnection=true";
 const connectToMongo = () => {
   mongoose.connect(mongoURI);
   console.log("Connected to Mongo Successfully");
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,20 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const { body, validationResult } = require('express-validator');
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 //Create a user using: POST "/api/auth". Doesn't require auth
 
-router.post("/", [
-  body('name').isLength({min: 3}),
-  body('email').isEmail(),
-  body('password').isLength({min: 5}),
-],(req, res) => {
-  // console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-  res.json([]);
-});
+router.post(
+  "/",
+  [
+    body("name", 'Enter a valid name').isLength({ min: 3 }),
+    body("email",'Enter a valid email').isEmail(),
+    body("password",'Password must be atleast 5 characters').isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+    return ;
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err=> {console.log(err)
+  res.json({error: 'Please enter a unique value for email', message: err.message})
+})
+  }
+);
 
 module.exports = router;
