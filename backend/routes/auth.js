aec928b0b7b1b9d6482b722cb294f8d18d322881
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2023-07-15 16:24:07 +0530

    Login Endpoint added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = "thisismy_signature!";
 
@@ -32,28 +32,70 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this name already exists" });
       }
-      const salt = await bcrypt.genSalt(10); 
-      const secPass =await bcrypt.hash(req.body.password,salt) ;
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: secPass,
       });
       const data = {
-        user:{
-          id : user.id
-        }
-      }
-      const authToken =  jwt.sign(data,JWT_SECRET);
-      res.json({authToken});
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
     } catch (error) {
       console.log(error.message);
-      res.status(500).send("Some error occurred");
+      res.status(500).send("Internal Server Error : Some error occurred");
     }
     //   .then(user => res.json(user))
     //   .catch(err=> {console.log(err)
     // res.json({error: 'Please enter a unique value for email', message: err.message})
     // })
+  }
+);
+
+//Authenticate a user using: POST "/api/auth/login". No Login required
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    //if there are errors return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json("Please try to login with correct credentials");
+      }
+
+      const passComp = await bcrypt.compare(password, user.password);
+      if (!passComp) {
+        return res.status(400).json("Please enter the correct credentials");
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server Error : Some error occurred");
+    }
   }
 );
 
