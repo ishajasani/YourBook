dc3d3771df0ecbf1d587940575fd138b4297c795
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2023-07-15 22:34:39 +0530

    Update existing notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -7,7 +7,7 @@ const { body, validationResult } = require("express-validator");
 // ROUTE 1 : Get all notes : GET "/api/notes/fetchallnotes". Login required
 router.get("/fetchallnotes", fetchUser, async (req, res) => {
   try {
-    const notes = await Notes.find({ user: req.user.id });
+    const notes = await Note.find({ user: req.user.id });
     res.json(notes);
   } catch (error) {
     console.log(error.message);
@@ -50,4 +50,34 @@ router.post(
   }
 );
 
+// ROUTE 3 : Update an existing note : POST "/api/notes/updatenote". Login required
+router.put("/updatenote/:id", fetchUser, async (req, res) => {
+  try {
+    const { title, description, tag } = req.body;
+    //Create a new note object
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    //Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if(!note){
+      return res.status(404).send({error : "Note not found"});
+
+    }
+    if(note.user.toString() !== req.user.id){
+      return res.status(401).send({error : "Unauthorized"});
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id , {$set : newNote} , {new : true});
+    res.json({note});
+  } catch (error) {}
+});
 module.exports = router;
