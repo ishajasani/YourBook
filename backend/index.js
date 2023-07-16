3aedaf52d4097cf66ef43ab96ea131d84a064268
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2023-07-16 16:59:46 +0530

    Fetching Notes From Context

--- backend/index.js
+++ backend/index.js
@@ -15,5 +15,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`YourBook listening on port http://localhost:${port}`);
 });
--- src/context/Notes/NoteState.js
+++ src/context/Notes/NoteState.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import noteContext from "./NoteContext";
 
 const NoteState = (props) => {
-
+    const notesInitial = [
+        {
+          "_id": "64b3d2873803f8bc9b491ab5",
+          "user": "64b1521f999b818cabe60d4c",
+          "title": "first note",
+          "description": "helllo hello",
+          "tag": "new",
+          "date": "2023-07-16T11:20:39.035Z",
+          "__v": 0
+        }
+      ];
+      const [notes , setNotes] = useState(notesInitial)
   return (
-    <noteContext.Provider value={{}}>{props.children}</noteContext.Provider>
+    <noteContext.Provider value={{notes,setNotes}}>{props.children}</noteContext.Provider>
   );
 };
 
